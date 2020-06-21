7d45e239581e2fc55615ba63415b7d9259c86085
Jan <info@phoca.cz>
2020-06-21 14:00:15 +0200

    3.1.0

--- mod_phocaemail_newsletter.php
+++ mod_phocaemail_newsletter.php
@@ -7,8 +7,23 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
 defined('_JEXEC') or die('Restricted access');// no direct access
-if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+if (!JComponentHelper::isEnabled('com_phocaemail', true)) {
+
+	throw new Exception('Error - Phoca Email component needs to be installed', 500);
+	return false;
+}
+
+
+$lang = JFactory::getLanguage();
+//$lang->load('com_phocaemail.sys');
+$lang->load('com_phocaemail');
+JHtml::stylesheet('media/com_phocaemail/css/main.css' );
+require_once( JPATH_ADMINISTRATOR.'/components/com_phocaemail/helpers/phocaemailutils.php');
+require_once( JPATH_ADMINISTRATOR.'/components/com_phocaemail/helpers/phocaemaillists.php');
 require_once( JPATH_SITE.'/components/com_phocaemail/helpers/route.php');
+
+
 
 $user 		= JFactory::getUser();
 $db 		= JFactory::getDBO();
@@ -17,23 +32,36 @@ $menu  		= $app->getMenu();
 $document	= JFactory::getDocument();
 		
 // PARAMS
-$description 			= $params->get( 'description', '' );
-$display_mailing_list 	= $params->get( 'display_mailing_list', '' );
+$paramsC 				= JComponentHelper::getParams('com_phocaemail') ;
 
-$link = PhocaEmailHelperRoute::getNewsletterRoute(0, 'subscribe');
+$description 			= $params->get( 'description', '' );
+$display_mailing_list 	= $paramsC->get( 'display_mailing_list', 0 );
+$display_form_link		= $params->get( 'display_form_link', 1);
+$link_text				= $params->get( 'link_text', '');
+
+
+if ($link_text == '') {
+	$link_text = 'MOD_PHOCAEMAIL_NEWSLETTER_SUBSCRIBE_TO_OUR_NEWSLETTER';
+}
+$link_text = JText::_($link_text);// Translate even the string set by parameters
+
+$link = PhocaEmailHelperRoute::getNewsletterRoute(0);
+$linkSubscribe = PhocaEmailHelperRoute::getNewsletterRoute(0, 'subscribe');
 //echo JRoute::_($link);
 //$output = '<a href="'.JRoute::_($link).'">Unsubscribe</a>';
 $lists = array();
 if ($display_mailing_list == 1) {
-
+	if ($display_mailing_list == 1) {
+	//$lists = PhocaEmailListsHelper::options();
 	$order 	= 'ordering ASC';
 	$db 	= JFactory::getDBO();
 	$query 	= 'SELECT a.id AS value, a.title AS text'
 			.' FROM #__phocaemail_lists AS a'
 			.' WHERE a.published = 1'
-			. ' ORDER BY '. $order;
+			.' ORDER BY '. $order;
 	$db->setQuery($query);
 	$lists = $db->loadObjectList();
+}
 }
 
 
--- tmpl/default.php
+++ tmpl/default.php
@@ -8,73 +8,34 @@
  */
 defined('_JEXEC') or die('Restricted access');
 
+$layout 	= new JLayoutFile('newsletter_form', null, array('component' => 'com_phocaemail'));
 
 echo '<div class="pc-email-newsletter-mod">';
 
-echo '<form action="'.JRoute::_($link).'" method="post" id="ph-subscribe-form" class="form-inline">';
-if ($description != '') {
-	echo '<div class="pretext"><p>'.$description.'</p></div>';
-}
-echo '<div class="userdata">';
-
-echo '<div id="ph-form-subscribe-name" class="control-group">';
-echo '<div class="controls">';
-echo '<div class="input-prepend">';
-echo '<span class="add-on">';
-echo '<span class="glyphicon glyphicon-user icon-user hasTooltip" title="'. JText::_('MOD_PHOCAEMAIL_NEWSLETTER_NAME').'"></span>';
-echo '<label for="ph-mod-name" class="element-invisible">'. JText::_('MOD_PHOCAEMAIL_NEWSLETTER_NAME') .'</label>';
-echo '</span>';
-echo '<input id="ph-mod-name" type="text" name="name" class="input-small" tabindex="0" size="18" placeholder="'. JText::_('MOD_PHOCAEMAIL_NEWSLETTER_NAME') .'" aria-required="true" class="required" required />';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-
-
-echo '<div id="ph-form-subscribe-email" class="control-group">';
-echo '<div class="controls">';
-echo '<div class="input-prepend">';
-echo '<span class="add-on">';
-echo '<span class="glyphicon glyphicon-envelope icon-mail hasTooltip" title="'. JText::_('MOD_PHOCAEMAIL_NEWSLETTER_EMAIL').'"></span>';
-echo '<label for="ph-mod-email" class="element-invisible">'.  JText::_('MOD_PHOCAEMAIL_NEWSLETTER_EMAIL').'</label>';
-echo '</span>';
-echo '<input id="ph-mod-email" type="email" name="email" class="input-small required" tabindex="0" size="18" placeholder="'. JText::_('MOD_PHOCAEMAIL_NEWSLETTER_EMAIL') .'" aria-required="true"  required />';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-
-if (!empty($lists)) {
-	echo '<div id="ph-form-subscribe-mailinglist" class="control-group">';
-	echo '<div class="controls" style="margin:10px;">';
-	
-	foreach($lists as $k => $v) {
-		echo '<div class="checkbox">';
-		echo '<label>';
-		echo '<input type="checkbox" name="mailinglist[]" value="'.(int)$v->value.'"> '.$v->text;
-		echo '</label>';
-		echo '</div>';
-		echo '<div style="clear:both"></div>';
+	if ($description != '') {
+		echo '<div class="pretext"><p>'.$description.'</p></div>';
 	}
-	echo '</div></div>';
-}
+	
+	if ($display_form_link == 2) {
+		
+		// LINK
+		echo '<div class=""><a href="'.JRoute::_($link).'">'.strip_tags($link_text).'</a></div>';
+	} else if ($display_form_link == 3) {
+		
+		// BUTTON
+		echo '<div class=""><a class="button btn btn-primary ph-email-mod-btn" href="'.JRoute::_($link).'">'.strip_tags($link_text).'</a></div>';
+		
+	} else {
+			
+		$d						= array();
+		$d['params']			= $paramsC;
+		$d['mailing_list']		= $lists;
+		$d['link_subscribe']	= $linkSubscribe;
+		$d['extension-type']	= 'mod';
+		echo $layout->render($d);
+		
+	}
 
-echo '<div id="ph-form-subscribe-submit" class="control-group">';
-echo '<div class="controls">';
-echo '<button type="submit" tabindex="0" name="submit" class="btn btn-primary">'. JText::_('MOD_PHOCAEMAIL_NEWSLETTER_SUBSCRIBE') .'</button>';
 echo '</div>';
-echo '</div>';
-
-echo '</div>';
-
-
-echo '<input type="hidden" name="option" value="com_phocaemail" />';
-echo '<input type="hidden" name="view" value="newsletter" />';
-echo '<input type="hidden" name="task" value="subscribe" />';
-echo  JHtml::_('form.token');
-
-echo '</form>';
-
-?>
-
-</div>
-<div style="clear:both"></div>
+echo '<div style="clear:both"></div>';
 
